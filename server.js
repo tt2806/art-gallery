8c3a8ac45dfb47f9ada79a222b0d40494e5d2923
Thiago Techera <thiagotechera2812@icloud.com>
2022-12-06 19:28:16 -0500

    ejjs

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('view-engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', { name: 'Thiago' })
 })
 
 app.listen(3000)

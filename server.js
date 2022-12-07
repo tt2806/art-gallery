1ce4a1632e684488426ee35d6ad6f4a71194bdef
Thiago Techera <thiagotechera2812@icloud.com>
2022-12-07 06:15:37 -0500

    make pages

--- server.js
+++ server.js
@@ -7,4 +7,16 @@ app.get('/', (req, res) => {
     res.render('index.ejs', { name: 'Thiago' })
 })
 
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+    
+})
+
 app.listen(3000)

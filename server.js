ead8a00ea6281c39b06799081b14e8ff5d7741a4
Thiago Techera <thiagotechera2812@icloud.com>
2022-12-08 08:00:28 -0500

    finished login!!!

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const users = []
+
 app.set('view-engine', 'ejs')
 
 app.get('/', (req, res) => {
@@ -11,12 +13,16 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
+app.post('/login', (req, res) => {
+
+})
+
 app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
 app.post('/register', (req, res) => {
-    
+    req.body.email
 })
 
 app.listen(3000)
